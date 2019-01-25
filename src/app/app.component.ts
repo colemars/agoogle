45e0e21676111adf35608341a027aee8f457da6e
Cole Marsteller <cgm32547@gmail.com>
2019-01-25 06:43:04 -0800

    add canvas and demo matterjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,64 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 }
+
+window.addEventListener('load', function() {
+
+  //Fetch our canvas
+  var canvas = document.getElementById('cv');
+
+  //Setup Matter JS
+  var engine = Matter.Engine.create();
+  var world = engine.world;
+  var render = Matter.Render.create({
+    canvas: canvas,
+    engine: engine,
+    options: {
+      width: 500,
+      height: 500,
+      background: 'black',
+      wireframes: false,
+      showAngleIndicator: false
+    }
+  });
+
+  //Add a ball
+  var ball = Matter.Bodies.circle(250, 250, 50, {
+    density: 0.04,
+    friction: 0.01,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+            fillStyle: '#F35e66',
+            strokeStyle: 'black',
+            lineWidth: 1
+        }
+  });
+  Matter.World.add(world, ball);
+
+  //Add a floor
+  var floor = Matter.Bodies.rectangle(250, 520, 500, 40, {
+    isStatic: true, //An immovable object
+    render: {
+      visible: false
+    }
+  });
+  Matter.World.add(world, floor);
+
+  //Make interactive
+  var mouseConstraint = Matter.MouseConstraint.create(engine, { //Create Constraint
+    element: canvas,
+    constraint: {
+      render: {
+            visible: false
+        },
+        stiffness:0.8
+      }
+  });
+  Matter.World.add(world, mouseConstraint);
+
+  //Start the engine
+  Matter.Engine.run(engine);
+  Matter.Render.run(render);
+
+});
