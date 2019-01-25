e410ef21292f02886bf80d99149e66d9424aff64
Cole Marsteller <cgm32547@gmail.com>
2019-01-25 15:47:16 -0800

    update on reading and brainstorming

--- src/app/sketch/sketch.component.ts
+++ src/app/sketch/sketch.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as Matter from 'matter-js';
-import Two from "two.js";
+import * as p5 from 'p5';
 
+import { GetElementsComponent } from '../get-elements/get-elements.component.js'
 
 @Component({
   selector: 'app-sketch',
@@ -24,35 +25,79 @@ export class SketchComponent implements OnInit {
     // this.Bodies = Matter.Bodies;
   }
 
-  static setup() {
-    let elem = document.getElementById('draw-shapes');
-    let params = { width: 285, height: 200 };
-    let two = new Two(params).appendTo(elem);
+  static drawTest() {
+    let Engine = Matter.Engine,
+      World = Matter.World,
+      Composites = Matter.Composites,
+      Composite = Matter.Composite,
+      Body = Matter.Body,
+      Bodies = Matter.Bodies,
+      Events = Matter.Events,
+      MouseConstraint = Matter.MouseConstraint,
+      Mouse = Matter.Mouse;
 
-    var circle = two.makeCircle(72, 100, 50);
-var rect = two.makeRectangle(213, 100, 100, 100);
+    let delta = [ 0, 0 ];
+    let stage = [ window.screenX, window.screenY, window.innerWidth, window.innerHeight ];
+    this.getBrowserDimensions(stage, delta);
 
-// The object returned has many stylable properties:
-circle.fill = '#FF8000';
-circle.stroke = 'orangered'; // Accepts all valid css color
-circle.linewidth = 5;
-
-rect.fill = 'rgb(0, 200, 255)';
-rect.opacity = 0.75;
-rect.noStroke();
-
-// Don't forget to tell two to render everything
-// to the screen
-two.update();
-
-    let Engine = Matter.Engine;
-    let World = Matter.World;;
-    let Bodies = Matter.Bodies;
     let engine = Engine.create();
-    let world = engine.world;
-    let box =  Bodies.rectangle(400, 200, 80, 80);
-    console.log(box);
-    Engine.run(engine);
+
+    GetElementsComponent.get()
+  }
+
+  static getBrowserDimensions(stage, delta) {
+
+  var changed = false;
+
+  if ( stage[0] != window.screenX ) {
+
+    delta[0] = (window.screenX - stage[0]) * 50;
+    stage[0] = window.screenX;
+    changed = true;
+  }
+
+  if ( stage[1] != window.screenY ) {
+
+    delta[1] = (window.screenY - stage[1]) * 50;
+    stage[1] = window.screenY;
+    changed = true;
+  }
+
+  if ( stage[2] != window.innerWidth ) {
+
+    stage[2] = window.innerWidth;
+    changed = true;
+  }
+
+  if ( stage[3] != window.innerHeight ) {
+
+    stage[3] = window.innerHeight;
+    changed = true;
+  }
+
+  return changed;
+}
+
+  // static drawThings() {
+  //   let s = (sketch) => {
+  //     sketch.setup = () => {
+  //     sketch.createCanvas(window.innerWidth,window.innerHeight*2);
+  //     sketch.background(40);
+  //     };
+  //     sketch.draw = () => {
+  //
+  //     }
+  //   };
+  //   const P5 = new p5(s);
+    //
+    // let Engine = Matter.Engine;
+    // let World = Matter.World;;
+    // let Bodies = Matter.Bodies;
+    // let engine = Engine.create();
+    // let world = engine.world;
+    // let box =  Bodies.rectangle(400, 200, 80, 80);
+    // console.log(box);
+    // Engine.run(engine);
   }
 
 
