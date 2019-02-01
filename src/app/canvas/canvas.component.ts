1a699de576541442176d038d9b9ac93d9a6c6474
Cole Marsteller <cgm32547@gmail.com>
2019-02-01 14:35:10 -0800

    place self back in business

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as Matter from 'matter-js';
 import * as p5 from 'p5';
+import { Box } from '../models/box.model'
 
 @Component({
   selector: 'app-canvas',
@@ -14,6 +15,18 @@ export class CanvasComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    let Engine = Matter.Engine,
+        // Render = Matter.Render,
+        World = Matter.World,
+        Bodies = Matter.Bodies;
+
+    let engine;
+    let world;
+    let boxes = [];
+
+    let ground;
+
     const sketch = (s) => {
 
       s.preload = () => {
@@ -21,13 +34,34 @@ export class CanvasComponent implements OnInit {
       }
 
       s.setup = () => {
-        s.createCanvas(window.innerWidth, window.innerHeight);
+        s.createCanvas(400, 400);
+        engine = Engine.create();
+        world = engine.world;
+        //Engine.run(engine);
+        var options = {
+        isStatic: true
+        }
+        ground = Bodies.rectangle(200, s.height, s.width, 100, options);
+
+        World.add(world, ground);
       };
+
+      s.mouseDragged = () => {
+        boxes.push(new Box(s.mouseX, s.mouseY, s.random(10, 40), s.random(10, 40), world));
+      }
 
       s.draw = () => {
         s.background(51);
-      };
-    }
+        Engine.update(engine);
+        for (var i = 0; i < boxes.length; i++) {
+        boxes[i].show(s);
+        }
+        s.noStroke(255);
+        s.fill(170);
+        // s.rectMode(CENTER);
+        s.rect(ground.position.x, ground.position.y, s.width, 100);
+        };
+      }
     this.p5 = new p5(sketch);
   }
 }
--- src/app/models/box.model.ts
+++ src/app/models/box.model.ts
@@ -11,13 +11,13 @@ export class Box {
 
   }
 
-  show() {
+  show(p5) {
 
 
        let pos = this.body.position;
        let angle = this.body.angle;
 
-       p5.Push();
+       p5.push();
        p5.translate(pos.x, pos.y);
        p5.rect(0,0, this.w, this.h);
        p5.pop();
