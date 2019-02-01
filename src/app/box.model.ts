7c8d97dc8ea4cbfe306be51237cc09ba2ddbd0a9
Cole Marsteller <cgm32547@gmail.com>
2019-02-01 12:40:47 -0800

    build box model and add show() method

--- src/app/box.model.ts
+++ src/app/box.model.ts
@@ -1,4 +1,25 @@
+import * as Matter from 'matter-js';
+
 export class Box {
-  public done: boolean = false;
-  constructor(public description: string) { }
+  [x: string]: any;
+
+  constructor(x,y,w,h,world) {
+    this.body = Matter.Bodies.rectangle(x,y,w,h);
+    this.x = x;
+    this.y = y;
+    Matter.World.add(world, this.body);
+  }
+
+  ngOnInit() {
+  }
+
+  show(p5) {
+    const pos = this.body.position;
+    const angle = this.body.angle;
+
+    p5.push();
+    p5.translate(pos.x, pos.y);
+    p5.rect(0,0,this.w,this.h)
+    }
+
 }
