839662c82cc2f151ea7aaa20e9ae7fbb2f471688
Cole Marsteller <cgm32547@gmail.com>
2019-02-01 15:23:53 -0800

    render engine on screen

--- src/app/engine/engine.component.ts
+++ src/app/engine/engine.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Matter from 'matter-js';
 
 @Component({
   selector: 'app-engine',
@@ -10,6 +11,46 @@ export class EngineComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const Engine = Matter.Engine,
+          Render = Matter.Render,
+          Runner = Matter.Runner,
+          Composites = Matter.Composites,
+          Common = Matter.Common,
+          MouseConstraint = Matter.MouseConstraint,
+          Mouse = Matter.Mouse,
+          World = Matter.World,
+          Bodies = Matter.Bodies;
+
+
+    const engine = Engine.create();
+    const world = engine.world;
+
+    const render = Render.create({
+          element: document.body,
+          engine: engine,
+          options: {
+              width: window.innerWidth,
+              height: window.innerHeight,
+              background: '#0f0f13',
+              showAngleIndicator: false,
+              wireframes: false
+          }
+      });
+
+    Render.run(render);
+
+    const runner = Runner.create();
+    Runner.run(runner, engine);
+
+    const offset = 10,
+        options = {
+            isStatic: true
+    };
+
+    world.bodies = [];
+
+
   }
 
 }
