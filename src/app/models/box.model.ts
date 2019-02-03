4eef8d01635b04c4971bf3b7aae023ea5d2ccf3f
Cole Marsteller <cgm32547@gmail.com>
2019-02-03 02:25:31 -0800

    set imageMode and rectMode to center

--- src/app/models/box.model.ts
+++ src/app/models/box.model.ts
@@ -1,12 +1,13 @@
 import * as Matter from 'matter-js';
 import * as p5 from 'p5';
+// import * as p from '@types/p5';
+/// <reference path="../node_modules/@types/p5/index.d.ts"/>
 
 export class Box {
   [x: string]: any;
-  constructor(x, y, w, h, img, world) {
+  constructor(x, y, w, h, world) {
     this.w = w;
     this.h = h;
-    this.img = img;
     this.body = Matter.Bodies.rectangle(x,y,w,h)
     Matter.World.add(world, this.body);
 
@@ -20,7 +21,9 @@ export class Box {
 
        p5.push();
        p5.translate(pos.x, pos.y);
-       p5.image(this.img, 0, 0, this.w, this.h);
+       p5.rotate(angle);
+       p5.rectMode(p5.CENTER);
+       p5.rect(0,0, this.w, this.h);
        p5.pop();
 
 
