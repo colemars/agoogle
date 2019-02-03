1dede89ad64a6c6cae1c7c091fbcb9cb6ee94a0f
Cole Marsteller <cgm32547@gmail.com>
2019-02-03 12:25:12 -0800

    add boundary model

---
+++ src/app/models/boundary.model.ts
@@ -0,0 +1,42 @@
+import * as Matter from 'matter-js';
+import * as p5 from 'p5';
+
+export class Boundary {
+  [x: string]: any;
+  constructor(x, y, w, h, options, world) {
+    this.w = w;
+    this.h = h;
+    this.world = world;
+    this.options = {
+      friction: 0.3,
+      restitution: 0.6,
+      isStatic: true,
+    }
+
+    this.body = Matter.Bodies.rectangle(x,y,w,h,options)
+    Matter.World.add(this.world, this.body);
+
+  }
+
+  show(p5) {
+
+
+       let pos = this.body.position;
+       let angle = this.body.angle;
+
+       p5.push();
+       p5.translate(pos.x, pos.y);
+       p5.rotate(angle);
+       p5.rectMode(p5.CENTER);
+       p5.fill(0);
+       p5.rect(0,0, this.w, this.h);
+       p5.pop();
+
+
+  }
+
+  delete() {
+    Matter.World.remove(this.world,this.body)
+  }
+
+}
