2e2d6a437fd8d87286d089f80b4945fa8c9480b5
Cole Marsteller <cgm32547@gmail.com>
2019-02-03 16:41:21 -0800

    offset bounds to match input draw point

--- src/app/models/search.model.ts
+++ src/app/models/search.model.ts
@@ -10,35 +10,53 @@ export class Search {
     this.options = {
       friction: 0.3,
       restitution: 0.6,
-      isStatic: true,
+      isSleeping: true,
     }
 
-    this.body = Matter.Bodies.rectangle(x,y,w,1,this.options)
+    this.element;
+    this.body = Matter.Bodies.rectangle(x,y,w,h,this.options);
+    // this.body2 = Matter.Bodies.rectangle(x,y+h,w,h,this.options);
     Matter.World.add(this.world, this.body);
+    // Matter.World.add(this.world, this.body2);
+
+    this.body.position.x = this.body.bounds.min.x;
+    this.body.position.y = this.body.bounds.min.y;
+    this.body.positionPrev.x = this.body.bounds.min.x;
+    this.body.positionPrev.y = this.body.bounds.min.y;
+
 
   }
 
-  show(p5) {
+  build(p5) {
 
 
        let pos = this.body.position;
        let angle = this.body.angle;
 
-       let input;
+
 
 
 
            p5.push();
-           input = p5.createInput().id('search');
+           this.element = p5.createInput().id('search');
            // s.rectMode(s.CENTER);
            // p5.translate(pos.x, pos.y);
-           input.position(pos.x-this.w/2, pos.y);
+           this.element.position(pos.x, pos.y);
            document.getElementById('search').style.width = this.w + 'px';
            document.getElementById('search').style.height = this.h + 'px';
            // p5.rect(pos.x, pos.y, this.w, this.h);
            p5.pop();
 
 
+  }
+
+  show(p5) {
+    p5.push();
+    let pos = this.body.position;
+    let angle = this.body.angle;
+    p5.rotate(angle);
+    this.element.position(pos.x, pos.y);
+    p5.pop();
   }
 
   delete() {
