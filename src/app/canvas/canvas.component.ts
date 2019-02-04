d784b1e1b2680a3f4a71c350d1e80bd61759cb49
Cole Marsteller <cgm32547@gmail.com>
2019-02-03 18:06:17 -0800

    add all header items and physics functionalities - semi working state

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -6,6 +6,7 @@ import { Box } from '../models/box.model'
 import { Logo } from '../models/logo.model'
 import { Boundary } from '../models/boundary.model'
 import { Search } from '../models/search.model'
+import { HeaderItem } from '../models/header-item.model'
 
 @Component({
   selector: 'app-canvas',
@@ -29,71 +30,104 @@ export class CanvasComponent implements OnInit {
         searchBody,
         boxes = [],
         bodies = [],
-        logo,
+        contents = [],
         mousePressed = false,
         ground,
-        img;
+        logo,
+        logo_img,
+        gmail,
+        gmail_img,
+        images,
+        images_img,
+        menu,
+        menu_img,
+        sign_in,
+        sign_in_img,
+        about,
+        about_img,
+        store,
+        store_img;
 
     const sketch = (s) => {
 
       s.preload = () => {
 
-        img = s.loadImage('./assets/images/logo.png');
+        //google logo
+        logo_img = s.loadImage('./assets/images/logo.png');
+
+        //about text
+        about_img = s.loadImage('./assets/images/about.png');
+
+        store_img = s.loadImage('./assets/images/store.png');
+
+        gmail_img = s.loadImage('./assets/images/gmail.png');
+
+        images_img = s.loadImage('./assets/images/images.png');
+
+        menu_img = s.loadImage('./assets/images/menu.png');
+
+        sign_in_img = s.loadImage('./assets/images/sign_in.png');
+
+        //supposed to help performance
         s.disableFriendlyErrors = true
 
       }
 
       s.setup = () => {
+
+        //builds p5 canvas
         s.createCanvas(window.innerWidth,window.innerHeight);
+
+        //creates engine and world for matter.js physics bodies
         engine = Engine.create();
         world = engine.world;
 
+        //sets sleeping to true. Unique from static because sleeping can be toggled(awoke).
         var options = {
         isSleeping: true
         }
 
+        //builds sleeping search bar, could probably be refactored
         search = new Search(s.width/2, s.height/3+20, 500, 40, world, s);
         search.build(s)
-        let test2 = document.getElementById('search');
+
+
+        contents.push(
+          new HeaderItem(s.width-55,30,sign_in_img.width/2, sign_in_img.height/2, options, sign_in_img, world),
+          new HeaderItem(s.width-125,30,menu_img.width/2, menu_img.height/2, options, menu_img, world),
+          new HeaderItem(s.width-180,30,images_img.width/2, images_img.height/2, options, images_img, world),
+          new HeaderItem(s.width-240,30,gmail_img.width/2, gmail_img.height/2, options, gmail_img, world),
+          new HeaderItem(110,30,store_img.width/2, store_img.height/2, options, store_img, world),
+          new HeaderItem(50,30,about_img.width/2, about_img.height/2, options, about_img, world),
+          new Logo(s.width/2, s.height/4, logo_img.width/2, logo_img.height/2, options, logo_img, world)
+        )
+
+
+        console.log(contents)
 
 
 
-
-
-        logo = new Logo(s.width/2, s.height/4, img.width/2, img.height/2, options, img, world);
-
-
+        //builds ground
         ground = new Boundary(s.width/2, s.height+3, s.width, 100, {isStatic: true}, world);
-
-
-
       };
 
+      //wakes everything up and adds bodies to array
       s.mouseClicked = () => {
-
-        var testOptions = {
+        const logoArrayOptions = {
         isStatic: false
         }
-
-
-
-        if (!(mousePressed)) {
-        logo.body.isSleeping = false;
-
+        //sets all from contents array toSleep false
+        for (var i = 0; i < contents.length; i++) {
+          contents[i].body.isSleeping = false;
+        };
         search.body.isSleeping = false;
-
-        console.log(search);
-
-        }
-
-        mousePressed = true;
-
-        boxes.push(new Logo(s.mouseX+10, s.mouseY, img.width/10, img.height/10, testOptions, img, world));
-
+        boxes.push(new Logo(s.mouseX+10, s.mouseY, logo_img.width/10, logo_img.height/10, logoArrayOptions, logo_img, world));
       }
 
+
+      //canvas update
       s.draw = () => {
-        s.background(51);
+        s.background('white');
         s.noStroke(255);
         s.fill(170);
         Engine.update(engine);
@@ -101,11 +135,13 @@ export class CanvasComponent implements OnInit {
         isStatic: false
         }
 
-        logo.show(s)
+        //updates positions relative to matter bodies
         ground.show(s)
         search.show(s)
-
-
+        // shows all generated bodies from array
+        for (var i = 0; i < contents.length; i++) {
+          contents[i].show(s)
+        }
         for (var i = 0; i < boxes.length; i++) {
         boxes[i].show(s);
         }
